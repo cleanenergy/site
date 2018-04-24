8d19f3496b9a67f6a78c6bb2797f2ce7d983eb11
Clean Energy <37281648+cleanenergy@users.noreply.github.com>
2018-04-24 19:03:35 -0300

    commit

--- clean/medicao/povoador.py
+++ clean/medicao/povoador.py
@@ -7,12 +7,11 @@ import random
 
 
 
-def povoar(meses_antes=6, loops=24, delta_hora= 1, delta_dia=1, limite_random=0.2, pk_geradora=1, delete_all=False):
+def povoar(meses_antes=6, delta_hora= 1, limite_random=0.2, pk_geradora=1, delete_all=False):
 
 	geradora = Geradora.objects.get(pk=pk_geradora)
 	hoje = datetime.today()
 	medida_inicial = 0
-	d = timedelta(days=delta_dia)
 	h = timedelta(hours=delta_hora)
 
 	if delete_all:
@@ -23,12 +22,10 @@ def povoar(meses_antes=6, loops=24, delta_hora= 1, delta_dia=1, limite_random=0.
 	medida_atual = medida_inicial
 
 	while data_hora.strftime("%Y-%m-%d") != hoje.strftime("%Y-%m-%d"):
-		for i in range(0,loops):
-			medido = Medida(ug=geradora, data_hora=data_hora, medida=medida_atual)
-			medido.save()
-			if data_hora.hour < 6 or data_hora.hour > 17:
-				medida_atual = medida_atual + 0
-			else:
-				medida_atual = medida_atual + round(random.uniform(0,limite_random), 2)
-			data_hora = data_hora + h
-		data_hora = datetime(data_hora.year, data_hora.month, data_hora.day, 0, 0) + d
+		medido = Medida(ug=geradora, data_hora=data_hora, medida=medida_atual)
+		medido.save()
+		if data_hora.hour < 6 or data_hora.hour > 17:
+			medida_atual = medida_atual + 0
+		else:
+			medida_atual = medida_atual + round(random.uniform(0,limite_random), 2)
+		data_hora = data_hora + h
